73595018696563f76f8e2db58eefab2bd15078d8
Oleg Bondar <oleg@stroom.network>
2024-03-25 21:19:31 +0000

    Add keys generation test

--- go.mod
+++ go.mod
@@ -3,13 +3,17 @@ module github.com/stroomnetwork/keygen
 go 1.21.5
 
 require (
-	github.com/btcsuite/btcd/btcec/v2 v2.1.3
+	github.com/btcsuite/btcd/btcec/v2 v2.3.2
+	github.com/stretchr/testify v1.9.0
 	github.com/urfave/cli/v2 v2.27.1
 )
 
 require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ keygen_test.go
@@ -0,0 +1,26 @@
+package keygen
+
+import (
+	"encoding/hex"
+	"testing"
+
+	"github.com/btcsuite/btcd/btcec/v2"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGenerateRandomKeys(t *testing.T) {
+	keys, err := GenerateRandomKeys()
+	require.NoError(t, err)
+
+	decoded, err := hex.DecodeString(keys.PrivateKey)
+	require.NoError(t, err)
+	privateKey, publicKey := btcec.PrivKeyFromBytes(decoded)
+	require.False(t, HasOddY(publicKey))
+
+	privateKeyBytes := privateKey.Serialize()
+	publicKeyBytes := publicKey.SerializeCompressed()
+	require.Equal(t, keys.PrivateKey, hex.EncodeToString(privateKeyBytes))
+	require.Equal(t, keys.PublicKey, hex.EncodeToString(publicKeyBytes))
+	require.Equal(t, 33, len(publicKeyBytes))
+	require.Equal(t, 32, len(privateKeyBytes))
+}
